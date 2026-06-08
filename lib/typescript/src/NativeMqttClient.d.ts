ba4b984f93c498c67aaa6c25ef1626dda0885f9a
boboxiaodd <iam386@gmail.com>
2026-06-08 19:28:59 +0800

    add

---
+++ lib/typescript/src/NativeMqttClient.d.ts
@@ -0,0 +1,23 @@
+import { type TurboModule } from 'react-native';
+export interface ConnectOptions {
+    username: string;
+    password: string;
+    clientId: string;
+    cleanSession: boolean;
+    connectionTimeout: number;
+    keepAliveInterval: number;
+    isAutomaticReconnect: boolean;
+    maxReconnectDelay: number;
+}
+export interface Spec extends TurboModule {
+    connect(brokerUrl: string, options?: ConnectOptions): Promise<string>;
+    disconnect(): Promise<string>;
+    subscribe(topic: string, qos: number): Promise<string>;
+    unsubscribe(topic: string): Promise<string>;
+    publish(topic: string, message: string, qos: number): Promise<string>;
+    addListener(eventName: string): void;
+    removeListeners(count: number): void;
+}
+declare const _default: Spec;
+export default _default;
+//# sourceMappingURL=NativeMqttClient.d.ts.map
