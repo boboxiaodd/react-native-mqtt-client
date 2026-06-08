00a95052097c070241ccb4bc881f18974bc81931
boboxiaodd <iam386@gmail.com>
2026-06-08 19:37:46 +0800

    Update index.d.ts

--- lib/typescript/src/index.d.ts
+++ lib/typescript/src/index.d.ts
@@ -3,6 +3,18 @@ export interface MqttMessage {
     topic: string;
     message: string;
 }
+
+export interface ConnectOptions {
+  username: string;
+  password: string;
+  clientId: string;
+  cleanSession: boolean;
+  connectionTimeout: number;
+  keepAliveInterval: number;
+  isAutomaticReconnect: boolean;
+  maxReconnectDelay: number;
+}
+
 export declare const Mqtt: {
     connect: (brokerUrl: string, options: {
         username: string;
