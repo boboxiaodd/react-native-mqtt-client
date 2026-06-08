800beda11dcca3cad991530adab107f9bfdfd69c
boboxiaodd <iam386@gmail.com>
2026-06-08 19:27:19 +0800

    add
    add

--- lib/typescript/src/index.d.ts
+++ lib/typescript/src/index.d.ts
@@ -1,1 +1,24 @@
-
+export type MqttEvent = 'onMqttConnected' | 'onMqttDisconnected' | 'onMqttMessageReceived' | 'onMqttError' | 'onMqttSubscribed' | 'onMqttUnsubscribed';
+export interface MqttMessage {
+    topic: string;
+    message: string;
+}
+export declare const Mqtt: {
+    connect: (brokerUrl: string, options: {
+        username: string;
+        password: string;
+        clientId: string;
+        cleanSession: boolean;
+        connectionTimeout: number;
+        keepAliveInterval: number;
+        isAutomaticReconnect: boolean;
+        maxReconnectDelay: number;
+    }) => Promise<string>;
+    disconnect: () => Promise<string>;
+    subscribe: (topic: string, qos?: number) => Promise<string>;
+    unsubscribe: (topic: string) => Promise<string>;
+    publish: (topic: string, message: string, qos?: number) => Promise<string>;
+    addListener: (eventName: MqttEvent, callback: (data: any) => void) => import("react-native").EventSubscription;
+};
+export default Mqtt;
+//# sourceMappingURL=index.d.ts.map
